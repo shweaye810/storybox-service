6c75e3effd1dd9a8d4483d6ec65125ce630b0b0f
Shwe N Aye <shweaye810@gmail.com>
2019-06-16 22:10:32 -0500

    initial commit

---
+++ src/main/java/com/ss/storyboxservice/StoryboxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ss.storyboxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryboxServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryboxServiceApplication.class, args);
+	}
+
+}
